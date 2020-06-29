d4026d2758e012cb5fa3df5520a15ffa1815834d
Thierry Chantier <titimoby@users.noreply.github.com>
2020-06-29 12:54:06 +0200

    typo

--- src/ecrire_config.py
+++ src/ecrire_config.py
@@ -1,5 +1,5 @@
 import ujson
-config = {'ssid':'votre SSID', 'passwd':'votre mote de passe'}
+config = {'ssid':'votre SSID', 'passwd':'votre mot de passe'}
 with open('/flash/wifi_config.json', 'w') as fichier:
     ujson.dump(config, fichier)
 
